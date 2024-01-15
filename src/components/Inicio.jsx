c2985d89143b6ad850a3c05be7527797acb34f44
Elias <ecmapps@gmail.com>
2024-01-14 21:44:39 -0600

    Bug fix background inicio

--- src/components/Inicio.jsx
+++ src/components/Inicio.jsx
@@ -1,18 +1,16 @@
 import "../styles/inicio.css";
-import dental from "../assets/dental2.avif";
 import dental3 from "../assets/dental3.avif";
 import dental5 from "../assets/dental5.avif";
-import dental7 from "../assets/dental7.avif";
 
 export const Inicio = () => {
   return (
     <div className="inicio-bg">
-      <div className="bg-danube-200/75 animate-fade">
+      <div className="danube-filter animate-fade-up lg:animate-none">
         <div className="drop-shadow-lg flex grow mt-1 px-2 py-4 h-[80svh]">
         <div className="flex flex-col lg:flex-row w-full">
           <section className="my-4 h-[80svh] lg:w-1/2 flex flex-col items-start justify-center px-4 gap-4">
             <h1 className="text-4xl font-bold md:text-6xl animate-fade text-gray-900 text-balance animate-fade-up">
-              Más de <span className="text-danube-800/80">20 años</span> de
+              Más de <span className="text-danube-700 lg:text-danube-800/80">20 años</span> de
               experiencia
             </h1>
             <p className="text-xl leading-8 text-gray-700 animate-fade-up animate-delay-[250ms]">
