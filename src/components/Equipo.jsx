d61564a7f193fe536dcb6a18fabd886998d3088e
Anggie Alava <anggie.alava.12@gmail.com>
2024-01-18 09:25:39 -0500

    probando rama develop gitlab

--- src/components/Equipo.jsx
+++ src/components/Equipo.jsx
@@ -32,7 +32,7 @@ export default function Equipo() {
       <div className="mx-auto grid max-w-7xl gap-x-8 gap-y-20 px-6 lg:px-8 xl:grid-cols-3">
         <div className="max-w-5xl">
           <h2 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-5xl pt-10 text-center">
-            Conoce a <span className="text-danube-600">nuestro equipo</span>
+            Este es <span className="text-danube-600">nuestro equipo</span>
           </h2>
           <p className="mt-6 text-2xl text-gray-800 text-center">
             Nuestro equipo de profesionales especializados en la salud bucal
