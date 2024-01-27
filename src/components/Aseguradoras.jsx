0382d030d3339c37731271006185a73638400c22
Elias <ecmapps@gmail.com>
2024-01-26 20:35:52 -0600

    Cambios menores en frases

--- src/components/Aseguradoras.jsx
+++ src/components/Aseguradoras.jsx
@@ -9,7 +9,7 @@ export const Aseguradoras = () => {
     <div className="bg-white py-6 sm:py-8">
       <div className="mx-auto max-w-10xl">
         <h2 className="text-center text-5xl font tracking font-semibold leading-10 text-gray-600">
-          Contamos con los mejores seguros médicos
+          Trabajamos con los mejores seguros médicos
         </h2>
         <div className="mx-auto mt-10 grid max-w-lg grid-cols-4 items-center gap-x-8 gap-y-10 sm:max-w-xl sm:grid-cols-6 sm:gap-x-10 lg:mx-0 lg:max-w-none lg:grid-cols-5">
           <img
